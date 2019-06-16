2e8ca60af36b39f5e804b178d16916d69af7d599
Elliot Brazier <elliot.brazier@ndus.edu>
2019-06-16 18:12:50 -0500

    lake work

--- src/app/melee/melee.component.ts
+++ src/app/melee/melee.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { RandomService } from '../random.service';
-import { HttpClient} from '@angular/common/http';
-import {MatCheckbox} from '@angular/material/checkbox';
+import { HttpClient } from '@angular/common/http';
+import { MatCheckbox } from '@angular/material/checkbox';
 @Component({
   selector: 'app-melee',
   templateUrl: './melee.component.html',
@@ -9,201 +9,6 @@ import {MatCheckbox} from '@angular/material/checkbox';
 })
 export class MeleeComponent implements OnInit {
   image: string = "../assets/melee_menu.png"
-  //characters are 110x110
-  /* coordinates: ImageMapCoordinate[] = [
-  //   {
-  //     name: "Dr. Mario",
-  //     x: 3,
-  //     y: 12,
-  //     width: 110,
-  //     height: 110
-  //   },
-  //   {
-  //     name: "Mario",
-  //     x: 123,
-  //     y: 6,
-  //     width: 110,
-  //     height: 110
-  //   },
-  //   {
-  //     name: "Luigi",
-  //     x: 246,
-  //     y: 6,
-  //     width: 110,
-  //     height: 110
-  //   },
-  //   {
-  //     name: "Bowser",
-  //     x: 368,
-  //     y: 6,
-  //     width: 110,
-  //     height: 110
-  //   },
-  //   {
-  //     name: "Peach",
-  //     x: 490,
-  //     y: 6,
-  //     width: 110,
-  //     height: 110
-  //   },
-  //   {
-  //     name: "Yoshi",
-  //     x: 615,
-  //     y: 6,
-  //     width: 110,
-  //     height: 110
-  //   },
-  //   {
-  //     name: "Donkey Kong",
-  //     x: 738,
-  //     y: 6,
-  //     width: 110,
-  //     height: 110
-  //   },
-  //   {
-  //     name: "Captain Falcon",
-  //     x: 860,
-  //     y: 6,
-  //     width: 110,
-  //     height: 110
-  //   },
-  //   {
-  //     name: "Ganondorf",
-  //     x: 980,
-  //     y: 13,
-  //     width: 110,
-  //     height: 110
-  //   },
-  //   {
-  //     name: 'Falco',
-  //     x: 3,
-  //     y: 138,
-  //     width: 110,
-  //     height: 110
-  //   },
-  //   {
-  //     name: "Fox",
-  //     x: 123,
-  //     y: 132,
-  //     width: 110,
-  //     height: 110
-  //   },
-  //   {
-  //     name: "Ness",
-  //     x: 246,
-  //     y: 132,
-  //     width: 110,
-  //     height: 110
-  //   },
-  //   {
-  //     name: "Ice Climbers",
-  //     x: 368,
-  //     y: 132,
-  //     width: 110,
-  //     height: 110
-  //   },
-  //   {
-  //     name: "Kirby",
-  //     x: 490,
-  //     y: 132,
-  //     width: 110,
-  //     height: 110
-  //   },
-  //   {
-  //     name: "Samus",
-  //     x: 615,
-  //     y: 132,
-  //     width: 110,
-  //     height: 110
-  //   },
-  //   {
-  //     name: "Zelda",
-  //     x: 738,
-  //     y: 132,
-  //     width: 110,
-  //     height: 110
-  //   },
-  //   {
-  //     name: "Link",
-  //     x: 860,
-  //     y: 132,
-  //     width: 110,
-  //     height: 110
-  //   },
-  //   {
-  //     name: "Young Link",
-  //     x: 980,
-  //     y: 137,
-  //     width: 110,
-  //     height: 110
-  //   },
-  //   {
-  //     name: "Free Space",
-  //     x: 5,
-  //     y: 255,
-  //     width: 110,
-  //     height: 110
-  //   },
-  //   {
-  //     name: "Pichu",
-  //     x: 125,
-  //     y: 262,
-  //     width: 110,
-  //     height: 110
-  //   },
-  //   {
-  //     name: "Pikachu",
-  //     x: 245,
-  //     y: 258,
-  //     width: 110,
-  //     height: 110
-  //   },
-  //   {
-  //     name: "Jigglypuff",
-  //     x: 368,
-  //     y: 257,
-  //     width: 110,
-  //     height: 110
-  //   },
-  //   {
-  //     name: "Mewtwo",
-  //     x: 492,
-  //     y: 258,
-  //     width: 110,
-  //     height: 110
-  //   },
-  //   {
-  //     name: "Mr GNW",
-  //     x: 614,
-  //     y: 258,
-  //     width: 110,
-  //     height: 110
-  //   },
-  //   {
-  //     name: "Marth",
-  //     x: 737,
-  //     y: 258,
-  //     width: 110,
-  //     height: 110
-  //   },
-  //   {
-  //     name: "Roy",
-  //     x: 859,
-  //     y: 262,
-  //     width: 110,
-  //     height: 110
-  //   },
-  //   {
-  //     name: "Sheik",
-  //     x: 980,
-  //     y: 259,
-  //     width: 110,
-  //     height: 110
-  //   },
-
-
-  // ]*/
-
   meleeChars = this.randomService.getMeleeChars();
   Opacity = "0.5";
   firstRoll = false;
@@ -212,170 +17,178 @@ export class MeleeComponent implements OnInit {
   toomanychars = [];
   disabledArray = [];
   public isCollapsed = true;
-  playerAImages = new Set();
-  playerBImages = new Set();
-  playerCImages = new Set();
-  playerDImages = new Set();
 
-  playerAChars = new Set();
-  playerBChars = new Set();
-  playerCChars = new Set();
-  playerDChars = new Set();
-  disabledChars = new Set();
-  overCharCount: boolean =false;
+  // playerAChars = new Set();
+  // playerBChars = new Set();
+  // playerCChars = new Set();
+  // playerDChars = new Set();
+  // disabledChars = new Set();
+  // overCharCount: boolean = false;
+   checked: boolean = false;
+  playerAShowCount: number;
+  playerBShowCount: number;
+  playerCShowCount: number;
+  playerDShowCount: number;
+  state = {
+    playerAChars: new Set(),
+    playerBChars: new Set(),
+    playerCChars: new Set(),
+    playerDChars: new Set(),
+    playerCount: 0,
+    charCount: 0,
+    playerAShowCount: this.playerAShowCount,
+    playerBShowCount: this.playerBShowCount,
+    playerCShowCount: this.playerCShowCount,
+    playerDShowCount: this.playerDShowCount,
+    disabledChars: new Set(),
+    checked: this.checked,
+    overCharCount: false
+  }
+
   constructor(private randomService: RandomService) {
     this.meleeChars = this.randomService.getMeleeChars();
   }
 
   ngOnInit() {
-    for (let x of this.meleeChars) {
-      console.log(x.name);
-    }
-    console.log(this.meleeChars);
-  }
-  randomFill() {
 
-    console.log();
-this.firstRoll = true;
-    this.charCount = ((document.getElementById("charCount")) as HTMLSelectElement).selectedIndex + 1;
-    this.playerCount = ((document.getElementById("playerCount")) as HTMLSelectElement).selectedIndex + 1;
-    this.playerAImages.clear();
-    this.playerAChars.clear();
-    this.playerBImages.clear();
-    this.playerBChars.clear();
-    this.playerCImages.clear();
-    this.playerCChars.clear();
-    this.playerDImages.clear();
-    this.playerDChars.clear();
-    this.overCharCount =false;
-    
-    if( this.meleeChars.length - this.disabledChars.size + 1 > this.charCount ){
-    while (this.playerAChars.size < this.charCount) {
-      var n = this.random();
-      console.log(n)
-    //  console.log(this.charCount)
-      var s = "";
-      for (let l of this.meleeChars) {
-        if (l.id == n && !this.disabledChars.has(l)) {
-          s = l.name;
-          this.playerAChars.add(s);
-        //  this.playerAImages.add(l.img);
-        }
-      }
-    }
+  }
+
   
 
-    if (this.playerCount >= 2) {
-      while (this.playerBChars.size < this.charCount) {
-        var n = this.random();
-        var s = "";
-        for (let l of this.meleeChars) {
-          if (l.id == n && !this.disabledChars.has(l)) {
-            s = l.name;
-            this.playerBChars.add(s);
-          //  this.playerBImages.add(l.img);
-          }
-        }
-      }
-    }
+  randomFill() {
 
-    if (this.playerCount >= 3) {
-      while (this.playerCChars.size < this.charCount) {
-        var n = this.random();
-        var s = "";
-        for (let l of this.meleeChars) {
-          if (l.id == n && !this.disabledChars.has(l)) {
-            s = l.name;
-            this.playerCChars.add(s);
-           // this.playerCImages.add(l.img);
-          }
-        }
-      }
-    }
-    if (this.playerCount == 4) {
-      while (this.playerDChars.size < this.charCount) {
-        var n = this.random();
-        var s = "";
-        for (let l of this.meleeChars) {
-          if (l.id == n && !this.disabledChars.has(l)) {
-            s = l.name;
-            this.playerDChars.add(s);
-         //   this.playerDImages.add(l.img);
-          }
-        }
-      }
-    }
+    console.log(this.checked)
+    //if(== true )  
+    this.firstRoll = true;
+    this.state.charCount = ((document.getElementById("charCount")) as HTMLSelectElement).selectedIndex + 1; //set char count
+    this.state.playerCount = ((document.getElementById("playerCount")) as HTMLSelectElement).selectedIndex + 1; //set the player count
+    this.state.playerAChars.clear();
+    this.state.playerBChars.clear(); //clear they shits
+    this.state.playerCChars.clear();
+    this.state.playerDChars.clear();
+    this.state.overCharCount = false;
 
-    this.shuffle(this.meleeChars);
-    for(let x of this.meleeChars){
-      if (this.playerAChars.has(x.name)) {
-        this.playerAImages.add(x.img);
+
+    if (this.meleeChars.length - this.state.disabledChars.size + 1 > this.state.charCount) { // if the whitelisted char count is under the allowed count
+      while (this.state.playerAChars.size < this.state.charCount) { // while the set is not filled
+        var n = this.random();
+        this.shuffle(this.meleeChars);
+        for (let l of this.meleeChars) {
+          if (l.id == n && !this.state.disabledChars.has(l.id)) {
+            this.state.playerAChars.add(l);
+          }
+        }
       }
-    }
-    this.shuffle(this.meleeChars);
-    for(let x of this.meleeChars){
-      if (this.playerBChars.has(x.name)) {
-        this.playerBImages.add(x.img);
+
+      if (this.state.playerCount >= 2) {
+        while (this.state.playerBChars.size < this.state.charCount) {
+          var n = this.random();
+          this.shuffle(this.meleeChars);
+          for (let l of this.meleeChars) {
+            if (l.id == n && !this.state.disabledChars.has(l.id)) {
+              this.state.playerBChars.add(l);
+            }
+          }
+        }
       }
-    }
-    this.shuffle(this.meleeChars);
-    for(let x of this.meleeChars){
-      if (this.playerCChars.has(x.name)) {
-        this.playerCImages.add(x.img);
+
+      if (this.state.playerCount >= 3) {
+        while (this.state.playerCChars.size < this.state.charCount) {
+          var n = this.random();
+          this.shuffle(this.meleeChars);
+          for (let l of this.meleeChars) {
+            if (l.id == n && !this.state.disabledChars.has(l.id)) {
+              this.state.playerCChars.add(l);
+            }
+          }
+        }
       }
-    }
-    this.shuffle(this.meleeChars);
-    for(let x of this.meleeChars){
-      if (this.playerDChars.has(x.name)) {
-        this.playerDImages.add(x.img);
+      if (this.state.playerCount == 4) {
+        while (this.state.playerDChars.size < this.state.charCount) {
+          var n = this.random();
+          this.shuffle(this.meleeChars);
+          for (let l of this.meleeChars) {
+            if (l.id == n && !this.state.disabledChars.has(l.id)) {
+              this.state.playerDChars.add(l);
+            }
+          }
+        }
       }
+      if (!this.state.checked) {
+        this.state.playerAShowCount = 30;
+        this.state.playerBShowCount = 30;
+        this.state.playerCShowCount = 30;
+        this.state.playerDShowCount = 30;
+      }
+      else {
+        this.state.playerAShowCount = 0;
+        this.state.playerBShowCount = 0;
+        this.state.playerCShowCount = 0;
+        this.state.playerDShowCount = 0;
+      }
+      console.log(this.state.playerAChars)
+      console.log(this.state.playerBChars)
     }
-   
+    else {
+
+      this.state.overCharCount = true;
+    }
   }
-  else{ 
-    
-    this.overCharCount = true;
+
+  advancePlayerA() {
+    if (this.state.playerAShowCount < this.state.playerAChars.size) {
+      this.state.playerAShowCount += 1;
+    }
   }
+
+  advancePlayerB() {
+    if (this.state.playerBShowCount < this.state.playerBChars.size) {
+      this.state.playerBShowCount += 1;
+    }
+  }
+
+  advancePlayerC() {
+    if (this.state.playerCShowCount < this.state.playerCChars.size) {
+      this.state.playerCShowCount += 1;
+    }
+  }
+
+  advancePlayerD() {
+    if (this.state.playerDShowCount < this.state.playerDChars.size) {
+      this.state.playerDShowCount += 1;
+    }
   }
   random() {
     var min = 0;
     var max = 27;
     return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
   }
-  clearDisabled() {
-    this.disabledChars.clear();
-    this.disabledChars = new Set();
-    this.disabledArray = [];
-    
-  }
-  logTest(s: string) {
-    console.log(s)
-  }
+
   toggleChar(s: string) {
     for (let x of this.meleeChars) {
       if (s == x.name) {
-        if (!this.disabledChars.has(x)) { //x.name previously 
-          this.disabledChars.add(x);
-          console.log('added ' + x.name)
+        if (!this.state.disabledChars.has(x.id)) { //x.name previously 
+          this.state.disabledChars.add(x.id);
+          console.log('added ' + x.name + " " + x.id)
           document.getElementById(x.name).style.opacity = "0.3";
-          console.log(this.disabledChars);
-          console.log(this.disabledArray)
+          // console.log(this.disabledChars);
+          // console.log(this.disabledArray)
         }
         else {
-         
-         document.getElementById(x.name).style.opacity = "1";
-          this.disabledChars.delete(x);
+
+          document.getElementById(x.name).style.opacity = "1";
+          this.state.disabledChars.delete(x.id);
           console.log('removed ' + x.name)
-          console.log(this.disabledChars);
-          console.log(this.disabledArray)
+          console.log(this.state.disabledChars);
         }
       }
     }
-    this.disabledArray = Array.from(this.disabledChars);
-    
+
+
   }
-   shuffle(array) {
+
+  shuffle(array) {
     array.sort(() => Math.random() - 0.5);
   }
-  
+
 }
